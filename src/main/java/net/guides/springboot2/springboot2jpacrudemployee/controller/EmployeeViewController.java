439b35efa9b08ba205f088c11b6cd0e8bae40cfd
Nursyahira Sabrina <nursyahiras12@gmail.com>
2021-06-04 16:12:25 +0800

    Co-authored-by: live.com#keekelven@qynix.com <keekelven@qynix.com>

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudemployee/controller/EmployeeViewController.java
@@ -0,0 +1,35 @@
+package net.guides.springboot2.springboot2jpacrudemployee.controller;
+
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import net.guides.springboot2.springboot2jpacrudemployee.exception.ResourceNotFoundException;
+// import net.guides.springboot2.springboot2jpacrudemployee.model.Employee;
+// import net.guides.springboot2.springboot2jpacrudemployee.repository.EmployeeRepository;
+
+@Controller
+@RequestMapping("")
+public class EmployeeViewController {
+    @Autowired
+    // private EmployeeRepository employeeRepository;
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    // @ResponseBody
+    public String index() {
+        return "index";
+    }
+}
