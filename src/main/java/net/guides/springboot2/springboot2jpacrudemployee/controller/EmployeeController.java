4cd78730216728c051c884213455996cf9926157
AsmaSalsabilaYunos <asmasalsabilamy@gmail.com>
2021-06-05 20:36:25 +0800

    modify button position

--- src/main/java/net/guides/springboot2/springboot2jpacrudemployee/controller/EmployeeController.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudemployee/controller/EmployeeController.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.Valid;
+// import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
